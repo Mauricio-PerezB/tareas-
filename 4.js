c915d9d6baa4596a5a94f3a7d8d2b51f6087535e
Mauricio-PerezB <mauricio.perez2301@alumnos.ubiobio.cl>
2025-04-27 21:54:03 -0400

    archivo 4

---
+++ 4.js
@@ -0,0 +1,7 @@
+// Crear un array con 5 frutas
+let frutas = ["Manzana", "Banana", "Naranja", "Fresa", "Kiwi"];
+
+// Recorrer el array con un bucle for
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
