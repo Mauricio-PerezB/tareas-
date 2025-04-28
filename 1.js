9eba15c3ec11eaca7efc291fdf87c8b8e63fa08e
Mauricio-PerezB <mauricio.perez2301@alumnos.ubiobio.cl>
2025-04-27 21:08:12 -0400

    cambio al archivo

---
+++ 1.js
@@ -0,0 +1,9 @@
+
+let nombre = "Mauricio Pérez Baeza";  
+let edad = 22;            
+let tieneMascotas = true; 
+
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("¿Tiene mascotas?", tieneMascotas);
