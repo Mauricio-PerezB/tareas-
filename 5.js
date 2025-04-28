269b5763dbb395a2083fd3f7c8b5d18303bbd835
Mauricio-PerezB <mauricio.perez2301@alumnos.ubiobio.cl>
2025-04-27 21:59:04 -0400

    archivo tarea

---
+++ 5.js
@@ -0,0 +1,8 @@
+// Crear un array de 3 números
+let numeros = [1, 2, 3];
+
+// Usar map para multiplicar cada número por 2
+let numerosMultiplicados = numeros.map(num => num * 2);
+
+// Mostrar el nuevo array en la consola
+console.log(numerosMultiplicados); // [2, 4, 6]
