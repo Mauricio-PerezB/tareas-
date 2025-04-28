79a529da69704b036dbfda598b21bd48c9c0c2ac
Mauricio-PerezB <mauricio.perez2301@alumnos.ubiobio.cl>
2025-04-27 22:02:44 -0400

    archivo 7

---
+++ 7.js
@@ -0,0 +1,8 @@
+// Crear un array de números
+let numeros = [1, 3, 5, 7, 9, 2, 4, 6];
+
+// Usar filter para obtener los números mayores a 5
+let mayoresA5 = numeros.filter(num => num > 5);
+
+// Mostrar el nuevo array en la consola
+console.log(mayoresA5); // [7, 9, 6]
