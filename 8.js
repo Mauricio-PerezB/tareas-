fed03f705728f752007d5587a25aa200ea03f0fe
Mauricio-PerezB <mauricio.perez2301@alumnos.ubiobio.cl>
2025-04-27 22:04:21 -0400

    archivo 8

---
+++ 8.js
@@ -0,0 +1,8 @@
+// Crear un array de números
+let numeros = [1, 2, 3, 4, 5];
+
+// Usar reduce para obtener la suma total
+let sumaTotal = numeros.reduce((acumulador, num) => acumulador + num, 0);
+
+// Mostrar la suma total en la consola
+console.log(sumaTotal); // 15
