e5a518640ffa09c4d12b98afaba7368cbb8faf09
Mauricio-PerezB <mauricio.perez2301@alumnos.ubiobio.cl>
2025-04-27 21:21:55 -0400

    3.js

---
+++ 3.js
@@ -0,0 +1,8 @@
+// Función que recibe un nombre y devuelve un saludo
+function saludar(nombre) {
+    return `Hola, ${nombre}!`;
+}
+
+// Ejemplo de uso
+let saludo = saludar("Mauricio"); // Reemplaza  con el nombre que quieras 
+console.log(saludo); // Imprime 
