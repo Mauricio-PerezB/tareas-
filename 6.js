1591e04e1f9d7df966e0c2939b542550bb877691
Mauricio-PerezB <mauricio.perez2301@alumnos.ubiobio.cl>
2025-04-27 22:01:01 -0400

    archivo 6

---
+++ 6.js
@@ -0,0 +1,11 @@
+// Crear un array de objetos
+let personas = [
+    { nombre: "Juan", edad: 25 },
+    { nombre: "Ana", edad: 30 },
+    { nombre: "Carlos", edad: 22 }
+];
+
+// Usar forEach para mostrar el nombre de cada persona en la consola
+personas.forEach(persona => {
+    console.log(persona.nombre);
+});
